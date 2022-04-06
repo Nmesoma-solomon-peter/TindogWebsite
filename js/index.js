26ea5cd1357fb350c60c0cc5c41dbb306ca2840a
nmesoma <nmesomasolo2809@gmail.com>
2022-04-07 00:25:13 +0100

    modified index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
+// var twitteraddress = document.querySelectorAll("").setAttribute("href","");
 var facebookaddress = document.querySelectorAll("footer a")[1].setAttribute("href","https://web.facebook.com/peter.solomonnmesoma/");
-var facebookaddress = document.querySelectorAll("").setAttribute("href","");
-var facebookaddress = document.querySelectorAll("").setAttribute("href","");
+// var facebookaddress = document.querySelectorAll("").setAttribute("href","");
 var  emailaddress = document.querySelectorAll("footer a")[3].setAttribute("href","nmesomasolo2809@gmail.com");
 
